19fca02e409f2c018c18fbc5a8846a1c804ec6b8
AssertNadeem <nadeem.shine@gmail.com>
2024-06-21 09:22:42 +1200

    minor changes and updates

--- tests/02_post_todoitems.spec.js
+++ tests/02_post_todoitems.spec.js
@@ -34,13 +34,15 @@ test('Post API of ToDoItems - Negative tests', async ({ request }) => {
         }
     });
     console.log(await response.json());
+    //Assertions to check the status and responses.
     expect(response.status()).toBe(400);
     const responseBody = await response.json();
+    //Assertions to check the response body.
     expect(responseBody).toHaveProperty("title", "One or more validation errors occurred.");
 
 });
 
-//test 04
+//test 04 - This test has 2 API performed, one runs and provides the udid for the other
 test('Get API with Params multiple apis', async ({ request }) => {
     console.log("--------------------Starting test 03 -------------------");
 
@@ -52,6 +54,7 @@ test('Get API with Params multiple apis', async ({ request }) => {
     });
 
     console.log(await response.json());
+    //Assertions to check the status and responses.
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(201);
     const responseBody = await response.json();
@@ -61,9 +64,11 @@ test('Get API with Params multiple apis', async ({ request }) => {
     //Second API to pull the information based on the UDID
     const updateRequest = await request.get(`/api/todoItems/${udid}`);
     console.log(await updateRequest.json());
+    //Assertions to check the status and responses.
     expect(updateRequest.ok()).toBeTruthy();
     expect(updateRequest.status()).toBe(200);
     const updatedResponseBody = await updateRequest.json()
+    //Assertions to check the body has the properties.
     expect(updatedResponseBody).toHaveProperty("id", udid);
     expect(updatedResponseBody).toHaveProperty("isCompleted", false);
 
